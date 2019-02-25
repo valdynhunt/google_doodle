999f59bfabd4b5d28af124319a66594ea649258f
Valdyn Hunt <valdyn@mac.com>
2019-02-24 21:53:10 -0800

    Add random color on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 // Everything OK?
 console.log('OK');
+
+// get random number
+var limit = 3;
+var randNum = Math.floor( Math.random() * limit ) ;
+console.log( randNum );
+
+// Click Event
+$('#stage').click( function(){
+    $('body').addClass('color' + randNum);
+});
