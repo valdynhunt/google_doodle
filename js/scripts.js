b6347e187598828af3a2f95e7c4e506f3bda48a5
Valdyn Hunt <valdyn@mac.com>
2019-02-25 07:22:16 -0800

    change onclick color values to match logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,6 @@
 // Everything OK?
 console.log('OK');
 
-var color0 = 'blue';
-var color1 = 'red';
-var color2 = 'green';
-
 // get random number
 var limit = 3;
 var randNum = Math.floor( Math.random() * limit ) ;
