427d4d0b4d4aada99fbdcb9cd53f120231d42ea3
Valdyn Hunt <valdyn@mac.com>
2019-02-25 07:10:09 -0800

    add animation and color change on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 // Everything OK?
 console.log('OK');
 
+var color0 = 'blue';
+var color1 = 'red';
+var color2 = 'green';
+
 // get random number
 var limit = 3;
 var randNum = Math.floor( Math.random() * limit ) ;
 console.log( randNum );
 
+
 // Click Event
 $('#stage').click( function(){
-    $('body').addClass('color' + randNum);
-    $('#stage').addClass('moveme');
+    $('#gdrive').toggleClass('moveme');
+    $('.icon').toggleClass('color' + randNum);
 });
